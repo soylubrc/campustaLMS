d08c16d4181335e71d27f2cce63824a329c6393f
soylubrc <soylubrc@gmail.com>
2024-11-06 18:21:05 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,87 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ana Sayfa
+    |--------------------------------------------------------------------------
+    */
+
+    'home_title' => 'Ana Sayfa',
+
+    'slider_heading' => 'Çevrimiçi öðrenmenin keyfini çýkarýn',
+    'slider_hint' => "Çevrimiçi eðitim, geliþim için en iyi seçeneklerden biridir. \n Dünyanýn dört bir yanýndaki en iyi öðretmenlerden öðrenin",
+
+    'slider_search_placeholder' => 'Kurslarý, eðitmenleri ve organizasyonlarý arayýn...',
+    'find' => 'Ara',
+
+    'view_all' => 'Hepsini Gör',
+    // Ýstatistikler
+    'teachers' => 'Eðitmenler',
+    'skillful_teachers' => 'Yetkin Eðitmenler',
+    'skillful_teachers_hint' => 'Deneyimli eðitmenlerden öðrenmeye baþlayýn.',
+
+    'live_classes' => 'Canlý Dersler',
+    'live_classes_hint' => 'Canlý bilgi akýþýyla becerilerinizi geliþtirin.',
+
+    'happy_students' => 'Mutlu Öðrenciler',
+    'happy_students_hint' => 'Kurslarýmýza katýldýlar ve becerilerini geliþtirdiler.',
+
+    'offline_courses' => 'Video Kurslar',
+    'offline_courses_hint' => 'Coðrafi ve zaman kýsýtlamasý olmadan öðrenin.',
+
+    'hours' => 'Saatler',
+
+    'featured_classes' => 'Öne Çýkan Kurslar',
+    'featured_webinars' => 'Öne Çýkan Kurslar',
+    'featured_classes_hint' => '#Öne çýkan kurslara göz atýn ve beceri kazanýn',
+
+    'latest_classes' => 'En Yeni Kurslar',
+    'latest_webinars' => 'En Yeni Kurslar',
+    'latest_webinars_hint' => '#Yeni yayýmlanan kurslar',
+
+    'featured' => 'Öne Çýkan',
+    'downloadable' => 'Ýndirilebilir',
+    'download' => 'Ýndir',
+    'downloads' => 'Ýndirilenler',
+
+    'trending_categories' => 'Popüler Kategoriler',
+    'trending_categories_hint' => '#Popüler ve trend öðrenme konularýný keþfedin',
+
+    'best_sellers' => 'En Çok Satan Kurslar',
+    'best_sellers_hint' => '#En çok satan kurslardan öðrenin',
+
+    'best_rates' => 'En Ýyi Puanlanan Kurslar',
+    'best_rates_hint' => '#Yüksek kaliteli ve en iyi puanlanan içeriklerin keyfini çýkarýn',
+
+    'discount_classes' => 'Ýndirimli Kurslar',
+    'discount_classes_hint' => '#Kurslarý en son fiyatlarla edinin',
+
+    'free_classes' => 'Ücretsiz Kurslar',
+    'free_classes_hint' => '#Ücretsiz öðrenme fýrsatlarýný kaçýrmayýn',
+
+    'testimonials' => 'Görüþler',
+    'testimonials_hint' => '#Müþterilerimizin bizim hakkýmýzda söyledikleri',
+
+    'subscribe_now' => 'Þimdi Abone Ol!',
+    'subscribe_now_hint' => '#Bir abonelik planý seçin ve tasarruf edin!',
+
+    'instructors' => 'Eðitmenler',
+    'all_instructors' => 'Tüm Eðitmenler',
+    'instructors_hint' => '#Deneyimli ve yetkin eðitmenlerden öðrenin',
+    'reserve_a_live_class' => 'Canlý bir toplantý rezerve edin',
+
+    'organization' => 'Organizasyon',
+    'organizations' => 'Organizasyonlar',
+    'all_organizations' => 'Tüm Organizasyonlar',
+    'organizations_hint' => '#En iyi eðitim organizasyonlarý size yardým etmek için burada',
+
+    'blog' => 'Blog',
+    'all_blog' => 'Blog Yazýlarý',
+    'blog_hint' => '#Son haberleri ve makaleleri keþfedin',
+
+    'platform_address' => 'Adres',
+    'order_summary' => 'Sipariþ Özeti',
+  	'blog_search_placeholder' => 'Blog yazýlarýný ara...',
+];
